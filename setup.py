3f46df75fc4bde8750c4dcd0c72bfabcafdf67c3
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-12-02 10:46:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="digitaltwin-unique",  # Change this to a unique name
-    version="0.1.3",
-    description="A simple calculator package",
-    author="Akhi",
-    author_email="akhildevops15@gmail.com",
-    packages=find_packages(),  # Automatically include the 'calku' package
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    name='calculator',
+    version='1.0.3',  # Incremented version
+    packages=find_packages(),
+    install_requires=[
+        'pytest',
     ],
-    python_requires=">=3.6",
+    description='A calculator package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
