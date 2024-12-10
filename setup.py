0cf8168f3a03694f53354c999fa75433aeaaf2ef
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-12-10 14:12:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup, find_packages
 
+# Read the version from version.txt
+with open('version.txt') as f:
+    version = f.read().strip()
+
 setup(
     name='calculator',
-    version='1.0.26',  # Incremented version
+    version=version,  # Use the dynamically updated version
     packages=find_packages(),
     install_requires=[
         'pytest',
     ],
-    description='A calculator package',
+    description='Simple calculator package',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    options={
+        'sdist': {'formats': ['zip']}
+    },
 )
