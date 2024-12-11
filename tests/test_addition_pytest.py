e1887b39273e828b7e615108be5f66c767cd42a5
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-12-11 11:53:46 +0530

    Delete tests directory

--- tests/test_addition_pytest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from app import add  # Assuming your function is in app.py
-
-def test_add():
-    assert add(1, 2) == 3
-    assert add(-1, -1) == -2
-    assert add(0, 0) == 0
-    assert add(100, 200) == 300
