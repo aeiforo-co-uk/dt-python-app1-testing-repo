2ab02e28fe5d3b4855c947f71594c06c7fef047e
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-12-13 12:09:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -152,6 +152,6 @@ def get_book_requests():
     return jsonify(filtered_requests), 200
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
 
