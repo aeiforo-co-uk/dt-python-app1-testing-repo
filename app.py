95eadcb10002301431bece68f9c70aae2fa2416b
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-12-16 12:15:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={
-        'app_name': "Swagger API' Testing"
+        'app_name': "Swagger API' sTesting"
     }
 )
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
